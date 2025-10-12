0d9db4365a920e545eec7d666d1ac25d1729cd15
oozcan <onurozcan95@gmail.com>
2025-10-12 22:03:55 +0300

    Tutorial - 21

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -5,10 +5,27 @@ export default function App() {
   return (
     <View style={styles.container}>
         <Text>StyleSheet API</Text>
+        <View style={[styles.box, styles.lightblueBg]}>
+            <Text>Lightblue Box</Text>
+        </View>
+        <View style={[styles.box, styles.lightgreenBg]}>
+            <Text>Lightgreen Box</Text>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {flex: 1, backgroundColor: 'lightgreen',padding: 20}
+    container: {flex: 1, backgroundColor: '#bfbaba',padding: 20},
+    box: {
+        width: 200,
+        height: 100,
+        padding: 10
+    },
+    lightblueBg: {
+        backgroundColor: "lightblue",
+    },
+    lightgreenBg: {
+        backgroundColor: "lightgreen",
+    }
 })
