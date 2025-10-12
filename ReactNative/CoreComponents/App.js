54461316336b0b1d656b7586928a7901957a9b5f
oozcan <onurozcan95@gmail.com>
2025-10-12 20:28:07 +0300

    Tutorial - 10

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -1,9 +1,17 @@
-import { View,Text } from 'react-native';
+import { View,Text,Image,ImageBackground } from 'react-native';
+const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
   return (
     <View style={{flex: 1, backgroundColor: 'plum',paddingTop: 20}}>
+        {/*
         <Text><Text style={{color: 'white'}}>Hello</Text> World!</Text>
+        <Image source={logoImg} style={{width: 300, height: 300}}></Image>
+        <Image source={{uri: 'https://picsum.photos/300'}} style={{width: 300, height: 300}}></Image>
+        */}
+        <ImageBackground source={logoImg} style={{flex: 1}}>
+          <Text></Text>
+        </ImageBackground>
     </View>
   );
 }
