29ddb88b7cc947dc272e5eb2dd1bcc29f3ff18c8
oozcan <onurozcan95@gmail.com>
2025-10-12 20:16:22 +0300

    Tutoriel - 9

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -1,10 +1,9 @@
-import { View } from 'react-native';
+import { View,Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex: 1, backgroundColor: 'plum'}}>
-        <View style={{width: 200, height:200, backgroundColor: 'lightblue'}}></View>
-        <View style={{width: 200, height:200, backgroundColor: 'lightgreen'}}></View>
+    <View style={{flex: 1, backgroundColor: 'plum',paddingTop: 20}}>
+        <Text><Text style={{color: 'white'}}>Hello</Text> World!</Text>
     </View>
   );
 }
