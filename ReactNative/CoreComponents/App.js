8e617a81655068dae1baf2876a72b985bd3d97f5
oozcan <onurozcan95@gmail.com>
2025-10-13 22:23:37 +0300

    Tutorial - 36

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -31,3 +31,4 @@ const styles = StyleSheet.create({
 })
 // alignSelf: "flex-start" flex item'lara uygulanır. flex-end, center vs. kendi başlarına yönlemek için
 // flexBasis: content'e uygulanır. yükseklik gibi ama boşta kalan alanı orantısal olarak alır
+// flexShrink: content'e uygulanır. Yan yana iken box dışına taşıyorsa sığdırmak için 1 gibi değer verilir.
