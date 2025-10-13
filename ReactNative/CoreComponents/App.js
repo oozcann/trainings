c46d5b82056b69659aa75f0bd6504b1a9e5e37c1
oozcan <onurozcan95@gmail.com>
2025-10-13 22:02:55 +0300

    Tutorial - 29

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -5,16 +5,17 @@ export default function App() {
   
   return (
     <View style={styles.container}>
-        <Box style={{flex:1, backgroundColor: "green"}}>Box 1</Box>
-        <Box style={{flex:3, backgroundColor: "blue"}}>Box 2</Box>
-        <Box style={{flex:2, backgroundColor: "red"}}>Box 3</Box>
+        <Box style={{ backgroundColor: "green"}}>Box 1</Box>
+        <Box style={{ backgroundColor: "blue"}}>Box 2</Box>
+        <Box style={{ backgroundColor: "red"}}>Box 3</Box>
     </View>
   );
 }
 const styles = StyleSheet.create({
     container: {
         marginTop: 20,
-        flexDirection: "column-reverse", // column(defaulf), column-reverse,row,row-reverse
+        // flexDirection: "column-reverse", // column(defaulf), column-reverse,row,row-reverse
+        justifyContent: "space-evenly", // flex-start(default),center,flex-end,space-around, space-between,space-evenly
         borderWidth: 6,
         borderColor: "yellow",
         flex:1
