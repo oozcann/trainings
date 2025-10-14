0f50f2cc6f6b097da5fd7b83c0fdceea231be03a
oozcan <onurozcan95@gmail.com>
2025-10-14 21:48:05 +0300

    Tutorial - 46

--- ReactNative/CoreComponents/components/Pokemon/PokemonCard.js
+++ ReactNative/CoreComponents/components/Pokemon/PokemonCard.js
@@ -1,9 +1,84 @@
-import { View,Text } from "react-native";
+import { View,Text,StyleSheet,Platform } from "react-native";
 
 export default function PokemonCard () {
     return (
-        <View>
+        <View style={styles.card}>
             <Text>PokemonCard</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+  nameContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    marginBottom: 32,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  hp: {
+    fontSize: 22,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginBottom: 16,
+  },
+  typeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 40,
+  },
+  badge: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+    borderWidth: 4,
+  },
+  typeEmoji: {
+    fontSize: 30,
+    marginRight: 12,
+  },
+  typeText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  movesContainer: {
+    marginBottom: 12,
+  },
+  movesText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  weaknessContainer: {
+    marginBottom: 8,
+  },
+  weaknessText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
