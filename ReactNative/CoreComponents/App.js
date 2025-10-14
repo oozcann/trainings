3061e369ca31a39a81ada82ee9b9e321afc67f48
oozcan <onurozcan95@gmail.com>
2025-10-14 20:36:20 +0300

    Tutorial - 41

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -1,18 +1,35 @@
 import { View,Text,StyleSheet,Dimensions } from 'react-native';
+import { useState,useEffect } from 'react';
 import Box from './components/Box';
 
 export default function App() {
   
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get("window")
+  })
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener("change",({window}) => {
+      setDimensions({window});
+    })
+    return () => subscription?.remove();
+  })
+  const {window} = dimensions;
+  const deviceWidth = window.width;
+  const deviceHeight = window.height;
+console.log(deviceHeight,deviceWidth);
   return (
     <View style={styles.container}>
-        <View style={styles.box}>
-          <Text style={styles.text}>Welcome!</Text>
+        <View style={[styles.box, {
+          width: deviceWidth > 500 ? "70%" : "90%",
+          height: deviceHeight > 600 ? "60%" : "90%",
+        }]}>
+          <Text style={{fontSize: deviceWidth > 500 ? 50 : 24}}>Welcome!</Text>
         </View>
     </View>
   );
 }
-const deviceWidth = Dimensions.get("window").width;
-const deviceHeight = Dimensions.get("window").height;
+// const deviceWidth = Dimensions.get("window").width;
+// const deviceHeight = Dimensions.get("window").height;
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "plum",
@@ -21,13 +38,13 @@ const styles = StyleSheet.create({
         justifyContent: "center"
     },
     box: {
-      width: deviceWidth > 500 ? "%70" : "90%",
-      height: deviceHeight > 600 ? "60%" : "90%",
+      // width: deviceWidth > 500 ? "%70" : "90%",
+      // height: deviceHeight > 600 ? "60%" : "90%",
       backgroundColor: "lightblue",
       alignItems: "center",
       justifyContent: "center"
     },
     text: {
-      fontSize: deviceWidth > 500 ? 50 : 24
+      // fontSize: deviceWidth > 500 ? 50 : 24
     }
 })
