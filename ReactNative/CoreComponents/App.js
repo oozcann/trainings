18ff7f0549b500bf9722ca2c7cc128c5a4c4330b
oozcan <onurozcan95@gmail.com>
2025-10-14 22:04:34 +0300

    Tutorial - 47 - 48 -49 -50

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -1,15 +1,52 @@
-import { View,Text,StyleSheet,Platform } from 'react-native';
+import { ScrollView,StyleSheet,Platform } from 'react-native';
 import PokemonCard from './components/Pokemon/PokemonCard';
 import { SafeAreaProvider,SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+    const charmanderData = {
+        name: "Charmander",
+        image: require("./assets/charmander.png"),
+        type: "Fire",
+        hp: 39,
+        moves: ["Scratch", "Ember", "Growl", "Leer"],
+        weaknesses: ["Water", "Rock"],
+    };
 
+    const squirtleData = {
+        name: "Squirtle",
+        image: require("./assets/squirtle.png"), // Replace with the actual image path
+        type: "Water",
+        hp: 44,
+        moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+        weaknesses: ["Electric", "Grass"],
+    };
+
+    const bulbasaurData = {
+        name: "Bulbasaur",
+        image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+        type: "Grass",
+        hp: 45,
+        moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+        weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+    };
+
+    const pikachuData = {
+        name: "Pikachu",
+        image: require("./assets/pikachu.png"), // Replace with the actual image path
+        type: "Electric",
+        hp: 35,
+        moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+        weaknesses: ["Ground"],
+    };
   return (
     <SafeAreaProvider>
         <SafeAreaView style={styles.container}>
-            <View>
-                <PokemonCard></PokemonCard>
-            </View>
+            <ScrollView>
+                <PokemonCard {...charmanderData} />
+                <PokemonCard {...squirtleData} />
+                <PokemonCard {...bulbasaurData} />
+                <PokemonCard {...pikachuData} />
+            </ScrollView>
         </SafeAreaView>
     </SafeAreaProvider>
     
--- ReactNative/CoreComponents/components/Pokemon/PokemonCard.js
+++ ReactNative/CoreComponents/components/Pokemon/PokemonCard.js
@@ -1,10 +1,61 @@
-import { View,Text,StyleSheet,Platform } from "react-native";
+import { View,Text,StyleSheet,Platform,Image } from "react-native";
 
-export default function PokemonCard () {
+const getTypeDetails = (type) => {
+  switch (type.toLowerCase()) {
+    case "electric":
+      return { borderColor: "#FFD700", emoji: "⚡️" };
+    case "water":
+      return { borderColor: "#6493EA", emoji: "💧" };
+    case "fire":
+      return { borderColor: "#FF5733", emoji: "🔥" };
+    case "grass":
+      return { borderColor: "#66CC66", emoji: "🌿" };
+    default:
+      return { borderColor: "#A0A0A0", emoji: "❓" };
+  }
+};
+
+
+export default function PokemonCard ({
+    name,
+    image,
+    type,
+    hp,
+    moves,
+    weaknesses
+}) {
+    const { borderColor, emoji } = getTypeDetails(type);
     return (
         <View style={styles.card}>
-            <Text>PokemonCard</Text>
-        </View>
+            <View style={styles.nameContainer}>
+                <Text style={styles.name}>{name}</Text>
+                <Text style={styles.hp}>❤️HP: {hp}</Text>
+            </View>
+
+            <Image
+                source={image}
+                accessibilityLabel={`${name} Pokemon`}
+                style={styles.image}
+                resizeMode="contain"
+            />
+
+            <View style={styles.typeContainer}>
+                <View style={[styles.badge, { borderColor }]}>
+                    <Text style={styles.typeEmoji}>{emoji}</Text>
+                    <Text style={styles.typeText}>{type}</Text>
+                </View>
+            </View>
+
+            <View style={styles.movesContainer}>
+                <Text style={styles.movesText}>Moves: {moves.join(", ")}</Text>
+            </View>
+
+            <View style={styles.weaknessContainer}>
+                <Text style={styles.weaknessText}>
+                Weakness: {weaknesses.join(", ")}
+                </Text>
+            </View>
+    </View>
     )
 }
 
