0e864dbb5ce74d4f7e2a7b68f5fc4842a3e817a1
oozcan <onurozcan95@gmail.com>
2025-10-14 21:06:43 +0300

    Tutorial - 43

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -1,3 +1,47 @@
+import { View,Text,StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView, } from 'react-native-safe-area-context';
+
+export default function App() {
+
+  return (
+    <SafeAreaProvider>
+        <SafeAreaView style={styles.safeContainer}>
+            <View style={styles.container}>
+            <View style={styles.box}>
+                <Text style={styles.text}>Welcome!</Text>
+            </View>
+        </View>
+        </SafeAreaView>
+    </SafeAreaProvider >
+  );
+}
+
+const styles = StyleSheet.create({
+    safeContainer: {
+        flex: 1,
+        backgroundColor: "plum"
+    },
+    container: {
+        backgroundColor: "plum",
+        flex: 1,
+        alignItems: "center"
+    },
+    box: {
+      // padding: 20,
+    },
+    text: {
+      fontSize: 24
+    }
+})
+
+
+
+
+
+
+
+
+{/* Tutorial - 42 
 import { View,Text,StyleSheet,Dimensions,useWindowDimensions } from 'react-native';
 
 export default function App() {
@@ -37,3 +81,4 @@ const styles = StyleSheet.create({
       // fontSize: deviceWidth > 500 ? 50 : 24
     }
 })
+*/}    
