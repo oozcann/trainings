c8daa28b957fffde55ccfb7fe82afbee0911748a
oozcan <onurozcan95@gmail.com>
2025-10-15 23:06:46 +0300

    Tutorial - 65

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -20,6 +20,15 @@ export default function App() {
 
     return Object.keys(errors).length === 0;
   };
+
+  const handleSubmit = () => {
+    if (validateForm()) {
+      console.log("Submitted", username, password);
+      setUsername("");
+      setPassword("");
+      setErrors({});
+    }
+  };
   
   return (
     <SafeAreaProvider>
@@ -47,7 +56,7 @@ export default function App() {
                     {errors.password ? (
                     <Text style={styles.errorText}>{errors.password}</Text>
                     ) : null}
-                    <Button title='Login' onPress={() => {}}></Button>
+                    <Button title='Login' onPress={handleSubmit}></Button>
                 </View>
             </KeyboardAvoidingView>
             
