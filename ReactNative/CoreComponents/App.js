b01087bb16b8ee7525644b5d129ad01aa45c9cd4
oozcan <onurozcan95@gmail.com>
2025-10-15 22:36:15 +0300

    Tutorial - 61

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -1,10 +1,11 @@
-import { ScrollView,View,Text,StyleSheet,StatusBar, TextInput } from 'react-native';
+import { ScrollView,View,Text,StyleSheet,StatusBar, TextInput,Switch } from 'react-native';
 import { SafeAreaProvider,SafeAreaView } from 'react-native-safe-area-context';
 import { useState } from 'react';
 
 export default function App() {
     
-  const [name, setName] = useState("");  
+  const [name, setName] = useState("");
+  const [isDarkMode,setIsDarkMode] = useState(false);
     
   return (
     <SafeAreaProvider>
@@ -22,6 +23,15 @@ export default function App() {
                 ></TextInput>
             <Text style={{fontSize: 30,marginTop:20}}>Name is {name}</Text>
             <TextInput style={[styles.input,{height: 100}]} multiline/>
+            <View style={styles.switchContainer}>
+                <Text style={styles.text}>Dark Mode</Text>
+                <Switch 
+                    value={isDarkMode}
+                    onValueChange={() => setIsDarkMode((previousState) => !previousState)}
+                    trackColor={{false: "red", true: "darkblue"}}
+                    thumbColor="yellow"
+                />
+            </View>
         </SafeAreaView>
     </SafeAreaProvider>
     
@@ -38,5 +48,15 @@ const styles = StyleSheet.create({
         height: 40,
         marginTop: 10,
         borderWidth: 1
+    },
+    text: {
+        fontSize: 30,
+        padding: 10
+    },  
+    switchContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingHorizontal: 10
     }
 })  
