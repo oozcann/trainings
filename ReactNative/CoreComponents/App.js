032b51fa97b3dc80468f320ae7e62ecb2695edad
oozcan <onurozcan95@gmail.com>
2025-10-18 16:14:32 +0300

    Tutorial - 67

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -1,12 +1,46 @@
-import { ScrollView,View,Text,StyleSheet,StatusBar, TextInput,Switch, Button, Alert,Image,KeyboardAvoidingView, Platform } from 'react-native';
+import { FlatList,View,Text,StyleSheet,StatusBar, Button, Platform } from 'react-native';
 import { SafeAreaProvider,SafeAreaView } from 'react-native-safe-area-context';
-import { use, useState } from 'react';
+import { useState,useEffect } from 'react';
 
 export default function App() {
+  
+  const [postList, setPostList] = useState([]);
+
+  const fetchData = async (limit = 10) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
+    const data = await response.json();
+    setPostList(data);
+  }
+  
+  useEffect(() => {
+    fetchData();
+  },[]);
+
   return (
     <SafeAreaProvider>
         <SafeAreaView style={styles.container}>
-            <Text>Onur</Text>
+            <View style={styles.listContainer}>
+              <FlatList
+              data={postList}
+              renderItem={({item}) => {
+                return (
+                  <View style={styles.card}>
+                    <Text style={styles.nameText}>{item.title}</Text>
+                    <Text style={styles.typeText}>{item.body}</Text>
+                  </View>
+                )
+              }}
+              keyExtractor={(item) => item.id.toString()}
+              ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+              ListEmptyComponent={<Text>No Posts Found</Text>}
+              ListHeaderComponent={
+                <Text style={styles.headerText}>Post List</Text>
+              }
+              ListFooterComponent={
+                <Text style={styles.footerText}>End of list</Text>
+              }
+              />
+            </View>
         </SafeAreaView>
     </SafeAreaProvider>
     
@@ -15,8 +49,69 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 20,
-    backgroundColor: "#f5f5f5",
-  }
+    backgroundColor: "#F5F5F5",
+    paddingTop: StatusBar.currentHeight,
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center", // Center the loading spinner
+    alignItems: "center", // Center the loading spinner
+  },
+  listContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  nameText: {
+    fontSize: 30,
+  },
+  typeText: {
+    fontSize: 24,
+    color: "#666666",
+  },
+  headerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  footerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginTop: 12,
+  },
+  inputContainer: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 8,
+  },
+  errorContainer: {
+    backgroundColor: "#FFC0CB",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+    alignItems: "center",
+  },
+  errorText: {
+    color: "#D8000C",
+    fontSize: 16,
+    textAlign: "center",
+  },
 });
