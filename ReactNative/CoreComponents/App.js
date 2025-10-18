573e235322b1f8a191ac6bc5a29257d3756855bc
oozcan <onurozcan95@gmail.com>
2025-10-18 17:16:49 +0300

    Tutorial - 76

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -1,4 +1,4 @@
-import { View,Text,TextInput } from 'react-native';
+import { View,Text,TextInput,Pressable } from 'react-native';
 import { SafeAreaProvider,SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -11,8 +11,22 @@ export default function App() {
   
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='Home'>
-            <Stack.Screen name='Home' component={HomeScreen}/>
+        <Stack.Navigator initialRouteName='Home' screenOptions={{
+            headerStyle: { backgroundColor: "#6a51ae" },
+                    headerTitleStyle: { fontWeight: "bold" },
+                    headerTintColor: "#fff",
+                    contentStyle: { backgroundColor: "#e8e4f3" },
+                    headerRight: () => (
+                    <Pressable onPress={() => alert("Menu button pressed!")}>
+                        <Text style={{ color: "#fff", fontSize: 16 }}>Menu</Text>
+                    </Pressable>
+                    )
+        }}>
+            <Stack.Screen name='Home' component={HomeScreen}
+                options={{
+                    title: "Welcome Home"
+                }}
+            />
             <Stack.Screen name='About' component={AboutScreen}/>
         </Stack.Navigator>
     </NavigationContainer>
