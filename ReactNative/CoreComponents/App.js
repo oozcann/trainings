5ee4cc5e819ed805117ab74bf2b482bfbb69e02b
oozcan <onurozcan95@gmail.com>
2025-10-18 16:22:50 +0300

    Tutorial - 69

--- ReactNative/CoreComponents/App.js
+++ ReactNative/CoreComponents/App.js
@@ -6,6 +6,7 @@ export default function App() {
   
   const [postList, setPostList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
   const fetchData = async (limit = 10) => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
@@ -17,6 +18,12 @@ export default function App() {
   useEffect(() => {
     fetchData();
   },[]);
+
+  const handleRefresh = () => {
+    setRefreshing(true);
+    fetchData(20);
+    setRefreshing(false);
+  };
 
   if (isLoading) {
     return (
@@ -50,6 +57,8 @@ export default function App() {
               ListFooterComponent={
                 <Text style={styles.footerText}>End of list</Text>
               }
+              refreshing={refreshing}
+              onRefresh={handleRefresh}
               />
             </View>
         </SafeAreaView>
